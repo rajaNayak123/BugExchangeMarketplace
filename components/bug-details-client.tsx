1759033903b2d7c979c9c037d74fa32607bc767b
rajaNayak123 <nayakraja151@gmail.com>
2025-07-05 12:49:07 +0530

    complete bug details in client side

---
+++ components/bug-details-client.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { PaymentButton } from "@/components/payment-button"
+
+interface BugDetailsPaymentButtonProps {
+  bugId: string
+  amount: number
+}
+
+export function BugDetailsPaymentButton({ bugId, amount }: BugDetailsPaymentButtonProps) {
+  const handlePaymentSuccess = () => {
+    alert("Payment successful! Your bug bounty has been posted.")
+    window.location.reload()
+  }
+
+  return <PaymentButton bugId={bugId} amount={amount} onSuccess={handlePaymentSuccess} />
+}
